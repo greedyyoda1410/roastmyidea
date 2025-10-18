348a8645037806f78d1913f1e217ae28d2328b2d
Nahiyan Abu Bakr <nahiyanabubakr@Nahiyans-MacBook-Air-2.local>
2025-10-18 18:16:02 +0800

    fix: Update ESLint config to ignore test files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,10 @@ const eslintConfig = [
       "out/**",
       "build/**",
       "next-env.d.ts",
+      "tests/**",
+      "**/*.test.js",
+      "**/*.test.ts",
+      "drizzle/**",
     ],
   },
 ];
